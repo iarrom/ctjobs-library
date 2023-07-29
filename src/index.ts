3d6a48378a107858ad672df7bae81abbe15ff0f0
Iaroslav Romanov <54136840+iarrom@users.noreply.github.com>
2023-07-29 17:09:12 +0100

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
-import { greetUser } from '$utils/greet';
+import { Editor } from '@tiptap/core';
+import StarterKit from '@tiptap/starter-kit';
+import Typography from '@tiptap/extension-typography';
+import BubbleMenu from '@tiptap/extension-bubble-menu';
 
-window.Webflow ||= [];
-window.Webflow.push(() => {
-  const name = 'John Doe';
-  greetUser(name);
+new Editor({
+  editorelement: document.querySelector('.editor'),
+  extensions: [
+    StarterKit,
+    Typography,
+    BubbleMenu.configure({
+      element: document.querySelector('.editor-menu'),
+  ],
+  content: '<p>Hello World!</p>',
 });
